64be0e4492697ce5cf320b01aa0fabbb5aa4500e
raj9680 <aryanr222@gmail.com>
2024-03-20 16:12:44 +0530

    sliderr-fix

--- rep-client/src/app/app-routing.module.ts
+++ rep-client/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MultiSliderComponent } from './shared/multi-slider/multi-slider.component';
 
 const routes: Routes = [
-  { path: "", component: MultiSliderComponent },
+  // { path: "", component: MultiSliderComponent },
   { path: "", redirectTo: "views/home", pathMatch: "full" },
   { path: "views", redirectTo: "views/home", pathMatch: "full" },
   { path: "views", loadChildren: () => import("./views/views.module").then(m => m.ViewsModule) }
