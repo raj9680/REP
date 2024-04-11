bd671d083fa879f752ce8995ba005fe5b3385ee7
Varun <d.varun1207@gmail.com>
2024-04-11 22:11:38 +0530

    Advance search

--- rep-client/src/environments/environment.ts
+++ rep-client/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  // apiEndpoint: 'https://localhost:7047/'
-apiEndpoint: 'https://musing-lehmann.108-175-0-196.plesk.page/'
+   apiEndpoint: 'https://localhost:7047/'
+//apiEndpoint: 'https://musing-lehmann.108-175-0-196.plesk.page/'
 };
 
 /*
